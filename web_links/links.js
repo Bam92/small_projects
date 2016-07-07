e86b5b03f0c85a2463e63928d792c6211f0a4f17
Bam92 <me@abelmbula.url.ph>
2016-07-07 06:56:43 +0300

    Starting first project (a js programme to display links) with 3 initial files

---
+++ web_links/links.js
@@ -0,0 +1,22 @@
+// Liste of links to display. A link is definied by :
+// - its title
+// - its URL
+// - its author
+
+var listeLiens = [
+    {
+        title: "So Foot",
+        url: "http://sofoot.com",
+        author: "yann.usaille"
+    },
+    {
+        title: "Guide d'autodéfense numérique",
+        url: "http://guide.boum.org",
+        author: "paulochon"
+    },
+    {
+        title: "L'encyclopédie en ligne Wikipedia",
+        url: "http://Wikipedia.org",
+        author: "annie.zette"
+    }
+];
